3c259cc365568fc954c8376467fa6c14755f6014
Tyler <tanght19@gmail.com>
2018-04-05 23:34:57 +0800

    rxjs todos

---
+++ example/todos-rx/index.js
@@ -0,0 +1,80 @@
+let list = [
+  {
+    text: '明天去春游',
+    status: true
+  },
+  {
+    text: '山本',
+    status: false
+  }
+]
+
+const log = val => console.log(val)
+
+const biz = {
+  init() {
+    const $input = document.querySelector('.js-input')
+    const $btnAdd = document.querySelector('.js-add')
+    const $container = document.querySelector('.js-task-box')
+
+    const $stream = Rx.Observable.fromEvent($btnAdd, 'click')
+    .merge(
+      Rx.Observable.fromEvent($input, 'keyup')
+      .filter(e => e.keyCode === 13)
+    )
+    .map(() => $input.value)
+    .distinctUntilChanged()
+    .filter(val => val.trim() !== '')
+    .throttle(200)
+    .map(text => ({text, status: false}))
+    .merge(Rx.Observable.from(list))
+    .map(item => {
+      const ele = document.createElement('li')
+      ele.classList.add('list-group-item')
+      ele.innerHTML = this.createItemStr(item)
+      return ele
+    })
+    .do($item => {
+      $container.appendChild($item)
+      $input.value = ''
+    })
+    .replay(1)  // why?
+    .refCount()
+
+    $stream.mergeMap(this.toggleStream)
+    .merge($stream.mergeMap(this.removeStream))
+    .subscribe()
+  },
+  removeStream($item) {
+    const $remove = $item.querySelector('.js-remove')
+
+    return Rx.Observable.fromEvent($remove, 'click')
+    .do(() => {
+      $item.parentNode.removeChild($item)
+    })
+  },
+  toggleStream($item){
+    const $target = $item.querySelector('.js-item')
+
+    return Rx.Observable.fromEvent($target, 'click')
+    .map(e => e.target.classList)
+    .do((classList) => {
+      classList.contains('cls-active') ? classList.remove('cls-active') : classList.add('cls-active')
+    })
+    .map(() => $item)
+  },
+  createItemStr(item){
+    const cls = item.status ? 'cls-active' : ''
+    return `
+				<span class="task-item js-item ${cls}">${item.text}</span> 
+				<i class="icon-remove float-right js-remove">删除</i>
+		`
+  }
+}
+
+const createItem = item => {
+
+}
+
+
+biz.init()
--- example/todos-traditional/index.js
+++ example/todos-traditional/index.js
@@ -1,90 +1,118 @@
 let list = [
-	{
-		text: '明天去春游',
-		status: true
-	},
-	{
-		text: '山本',
-		status: false
-	}
+  {
+    text: '明天去春游',
+    status: true
+  },
+  {
+    text: '山本',
+    status: false
+  }
 ]
 
+const TodoModel = {
+  get() {
+    return list
+  },
+  add(text) {
+    list = list.concat({text: text.trim()})
+  },
+  remove(text) {
+    list = list.filter(item => item.text !== text)
+  },
+  update(text, idx) {
+    list = list.map((item, index) => {
+      if(idx === index){
+        return {
+          ...item,
+          text
+        }
+      }
+
+      return item
+    })
+  }
+}
+
 const biz = {
-	init() {
-		this.render()
-		this.addEvents()
-	},
-	addEvents() {
-		document.addEventListener('click', (e) => {
-			const target    = e.target,
-						classList = target.classList
+  init() {
+    this.render()
+    this.addEvents()
+  },
+  addEvents() {
+    document.addEventListener('click', (e) => {
+      const target    = e.target,
+            classList = target.classList
 
-			if (classList.contains('js-item')) {
-				this.toggleStatus(classList)
-			}
+      if (classList.contains('js-item')) {
+        this.toggleStatus(classList)
+      }
 
-			if (classList.contains('js-btn-add')) {
-				this.addItem()
-			}
+      if (classList.contains('js-add')) {
+        this.addItem()
+      }
 
-			if (classList.contains('js-remove')) {
-				this.removeItem(target.parentNode.children[0].innerText)
-			}
-		}, false)
+      if (classList.contains('js-remove')) {
+        this.removeItem(target.parentNode.children[0].innerText)
+      }
+    }, false)
 
-		document.addEventListener('keyup', (e) => {
-			const target = e.target
+    document.addEventListener('keyup', (e) => {
+      const target = e.target
 
-			if (target.classList.contains('js-input') && e.keyCode === 13) {
-				this.addItem()
-			}
-		}, false)
-	},
-	toggleStatus(classList) {
-		classList.contains('cls-active') ? classList.remove('cls-active') : classList.add('cls-active')
-	},
-	addItem() {
-		const input = document.querySelector('.js-input')
+      if (target.classList.contains('js-input') && e.keyCode === 13) {
+        this.addItem()
+      }
+    }, false)
+  },
+  toggleStatus(classList) {
+    classList.contains('cls-active') ? classList.remove('cls-active') : classList.add('cls-active')
+  },
+  addItem() {
+    const input = document.querySelector('.js-input')
 
-		if (!input.value || !input.value.trim() || list.some(item => item.text.trim() === input.value.trim())) {
-			input.value = ''
-			return false
-		}
+    if (!input.value || !input.value.trim() || list.some(item => item.text.trim() === input.value.trim())) {
+      input.value = ''
+      return false
+    }
 
-		list = list.concat({text: input.value.trim()})
-		this.render()
-	},
-	removeItem(text) {
-		list = list.filter(item => item.text !== text)
-		this.render()
-	},
-	render() {
-		const input = document.querySelector('.js-input')
-		const container = document.querySelector('.js-task-box')
-		const node = new DOMParser().parseFromString(this.getItem(list), 'text/html').body
+    TodoModel.add(input.value)
+    this.render()
+  },
+  removeItem(text) {
+    TodoModel.remove(text)
+    this.render()
+  },
+  updateItem(text, idx){
+    TodoModel.update(text, idx)
+    this.render()
+  },
+  render() {
+    const input = document.querySelector('.js-input')
+    const container = document.querySelector('.js-task-box')
+    const node = new DOMParser().parseFromString(this.getItem(TodoModel.get()), 'text/html').body
 
-		container.innerHTML = ''
-		input.value = ''
-		document.querySelector('.js-task-box').appendChild(node)
-	},
-	getItem(list) {
-		const cls = list.status ? 'cls-active' : ''
+    container.innerHTML = ''
+    input.value = ''
+    document.querySelector('.js-task-box').appendChild(node)
+  },
+  getItem(list) {
+    const cls = list.status ? 'cls-active' : ''
 
-		if (!list || list.length === 0) {
-			return '<li class="list-group-item">当前没有任务</li>'
-		}
+    if (!list || list.length === 0) {
+      return '<li class="list-group-item">当前没有任务</li>'
+    }
 
-		if (!Array.isArray(list)) {
-			return `
+    if (!Array.isArray(list)) {
+      return `
 			<li class="list-group-item">
 				<span class="task-item js-item ${cls}">${list.text}</span> 
 				<i class="icon-remove float-right js-remove">删除</i>
 			</li>
 			`
-		}
+    }
 
-		return list.map(item => this.getItem(item)).join('')
-	}
+    return list.map(item => this.getItem(item)).join('')
+  }
 }
 
 biz.init()
